e639b0703faa81b681beb08faa57e9db51c32835
sequence <richard.ellis@ahvla.gsi.gov.uk>
2014-10-22 09:37:38 +0100

    new batch mapping script added

---
+++ BatchMap.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+# script by ellisrichardj
+# This will perform mapping of all samples within a directory
+# Requires BWA and samtools
+
+# Version 0.1.1 21/10/14
+
+# 
+
+# check for mandatory positional parameters
+if [ $# -lt 2 ]; then
+  echo "
+Usage: $0 <path to reference> <path to Data Folder> "
+exit 1
+fi
+
+Start=$(date +%s)
+
+Ref=$1
+DataFolder="$2"
+Count=0
+threads=$(grep -c ^processor /proc/cpuinfo)
+
+	ref=$(basename "$Ref")
+	refname=${ref%%_*}
+	reffile=${ref%%.*}
+
+homedir=$(pwd)
+cp "$Ref" "$homedir"
+bwa index "$ref"
+
+for file in "$DataFolder"/*_*_*_R1_*.gz
+do
+	((Count=Count+1))
+	fname=$(basename "$file")
+	samplename=${fname%%_*}
+	mkdir "$samplename"_mapto_"$refname"
+	cd "$samplename"_mapto_"$refname"	
+	echo "Mapping sample "$Count": "$samplename" to "$ref""
+	bwa mem -t "$threads" "$homedir"/"$ref" "$DataFolder"/"$samplename"_*_*_R1_*.gz "$DataFolder"/"$samplename"_*_*_R2_*.gz | samtools view -Su - | samtools sort - "$samplename"-"$refname"_map_sorted
+	samtools index "$samplename"-"$refname"_map_sorted.bam
+	samtools flagstat "$samplename"-"$refname"_map_sorted.bam > "$samplename"-"$refname"_MappingStats.txt
+	cd "$homedir"	
+done
+
+End=$(date +%s)
+TimeTaken=$((End-Start))
+
+echo  | awk -v D=$TimeTaken '{printf "Mapped '$Count' samples in: %02d'h':%02d'm':%02d's'\n",D/(60*60),D%(60*60)/60,D%60}'
