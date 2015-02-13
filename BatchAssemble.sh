edba2c10cc0eb1f4e5d43ade383cd61f02894c76
ellisrichardj <richard.ellis@apha.gsi.gov.uk>
2015-02-13 15:39:44 +0000

    Altered wildcard usage for filename pattern

--- BatchAssemble.sh
+++ BatchAssemble.sh
@@ -4,15 +4,15 @@ set -e
 # script by ellisrichardj
 # This will perform denovo assembly on all samples within a directory
 # It works by running velvet with 2 different k-mer values and then merging these to produce a higher quality hybrid assembly
-# Requires Velvet_assemble.sh, Velvet, GARM and trimmomatic
+# Requires Velvet_assemble.sh, Velvet and trimmomatic
 # Velvet must have been compliped to allow longer k-mers and multi-threading
 
 # Version 0.1
 # Version 0.1.1 06/10/14 - added output for number of samples and time taken
 # Version 0.1.2 16/01/15 - added options to change velvet parameters (kmer and cutoff)
 #			 - performs second assembly with lower kmer (-30)
-#			 - uses GARM to merge assemblies
-# Version 0.1.3 21/01/15 - Collates assembly statistics (N50, largest contig, total size, etc) for all samples and k-mers 
+# Version 0.1.3 21/01/15 - Collates assembly statistics (N50, largest contig, total size, etc) for all samples and k-mers
+# Version 0.1.4 10/02/15 - Altered wildcard/filename pattern to allow for data merged with MergeFQ.sh 
 
 # set our defaults for the options
 KVALUE=101
@@ -45,14 +45,14 @@ Start=$(date +%s)
 echo "Sample_k-mer, Nodes, N50, MaxContig, TotalSize," >> AssemblyStats.csv
 Statsfile=$(readlink -f AssemblyStats.csv)
 
-for file in "$DataFolder"/*_*_*_R1_*.gz
+for file in "$DataFolder"/*_R1*.gz
 do
 	((Count=Count+1))
 	fname=$(basename "$file")
 	samplename=${fname%%_*}
 	mkdir "$samplename"_assembly
-	ln -s "$(readlink -f "$DataFolder"/"$samplename"_*_*_R1_*.gz)" "$samplename"_assembly/"$samplename"_R1.fastq.gz
-	ln -s "$(readlink -f "$DataFolder"/"$samplename"_*_*_R1_*.gz)" "$samplename"_assembly/"$samplename"_R2.fastq.gz
+	ln -s "$(readlink -f "$DataFolder"/"$samplename"*R1*.gz)" "$samplename"_assembly/"$samplename"_R1.fastq.gz
+	ln -s "$(readlink -f "$DataFolder"/"$samplename"*R1*.gz)" "$samplename"_assembly/"$samplename"_R2.fastq.gz
 	cd "$samplename"_assembly
 
 	echo "Assembling sample "$Count": "$samplename" with K=$KVALUE and cutoff=$CUTOFF"
@@ -65,8 +65,6 @@ do
 	Assembly2=$(readlink -f "$samplename"_"$LowerK"/*_contigs.fa)
 	echo -e "$Assembly2""\t"$samplename"_Velvet"$LowerK"" >> "$samplename"_assemblies.txt
 	awk '{ORS=""}/velvetg/{print $2","}/max/{print $4",",$9,$11,$13"\n"}' "$samplename"_"$LowerK"/*Log >> $Statsfile
-
-	GARM.pl -g "$samplename"_assemblies.txt -o "$samplename"_"$KVALUE"_"$LowerK"_merge
 
 	rm *.gz
 	cd ..
