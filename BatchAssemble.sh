0ede955a345eb9e0b2e6ad446d95c368e97e5ee0
ellisrichardj <richard.ellis@apha.gsi.gov.uk>
2015-03-03 23:46:10 +0000

    now uses CISA to merge assemblies

--- BatchAssemble.sh
+++ BatchAssemble.sh
@@ -3,16 +3,18 @@ set -e
 
 # script by ellisrichardj
 # This will perform denovo assembly on all samples within a directory
-# It works by running velvet with 2 different k-mer values and then merging these to produce a higher quality hybrid assembly
-# Requires Velvet_assemble.sh, Velvet and trimmomatic
-# Velvet must have been compliped to allow longer k-mers and multi-threading
+# It works by running velvet with 3 different k-mer values and then merging these to produce a higher quality hybrid assembly
+# Requires Velvet_assemble.sh, Velvet, CISA and trimmomatic
+# Velvet must have been compiled to allow longer k-mers and multi-threading
 
 # Version 0.1
 # Version 0.1.1 06/10/14 - added output for number of samples and time taken
 # Version 0.1.2 16/01/15 - added options to change velvet parameters (kmer and cutoff)
 #			 - performs second assembly with lower kmer (-30)
 # Version 0.1.3 21/01/15 - Collates assembly statistics (N50, largest contig, total size, etc) for all samples and k-mers
-# Version 0.1.4 10/02/15 - Altered wildcard/filename pattern to allow for data merged with MergeFQ.sh 
+# Version 0.1.4 10/02/15 - Altered wildcard/filename pattern to allow for data merged with MergeFQ.sh
+# Version 0.2.0 03/03/15 - Performs assemblies with three k-mer values and now uses CISA to merge assemblies #			generated with multiple k-mers, added comments throughout script to help understand
+#			the code
 
 # set our defaults for the options
 KVALUE=101
@@ -26,7 +28,8 @@ while getopts 'c:k:' opt ; do
   esac
 done
 
-LowerK=$((KVALUE-30))
+MidK=$((KVALUE-18))
+LowerK=$((KVALUE-36))
 
 # skip over the processed options
 shift $((OPTIND-1)) 
@@ -34,7 +37,7 @@ shift $((OPTIND-1))
 # check for mandatory positional parameters
 if [ $# -lt 1 ]; then
   echo "Usage: $0 {path to Data Folder} "
-  echo "Options: -k kmervalue (def: $KVALUE) | -c cov_cutoff (def: $CUTOFF)"
+  echo "Options: -k kmervalue (def: $KVALUE, min 61) | -c cov_cutoff (def: $CUTOFF)"
 exit 1
 fi
 
@@ -42,29 +45,61 @@ DataFolder="$1"
 Count=0
 Start=$(date +%s)
 
+# Prepare stats file
 echo "Sample_k-mer, Nodes, N50, MaxContig, TotalSize," >> AssemblyStats.csv
 Statsfile=$(readlink -f AssemblyStats.csv)
 
+# Run assembly for each sample
 for file in "$DataFolder"/*_R1*.gz
 do
 	((Count=Count+1))
 	fname=$(basename "$file")
 	samplename=${fname%%_*}
+# Create new directory for each sample
 	mkdir "$samplename"_assembly
 	ln -s "$(readlink -f "$DataFolder"/"$samplename"*R1*.gz)" "$samplename"_assembly/"$samplename"_R1.fastq.gz
 	ln -s "$(readlink -f "$DataFolder"/"$samplename"*R1*.gz)" "$samplename"_assembly/"$samplename"_R2.fastq.gz
 	cd "$samplename"_assembly
 
+# Assemble sample using highest k-mer value
 	echo "Assembling sample "$Count": "$samplename" with K=$KVALUE and cutoff=$CUTOFF"
-	Velvet_assemble.sh -k "$KVALUE" -c "$CUTOFF" "$samplename" "$samplename"_R1.fastq.gz "$samplename"_R2.fastq.gz
-	Assembly1=$(readlink -f "$samplename"_"$KVALUE"/*_contigs.fa)
-	echo -e "$Assembly1""\t"$samplename"_Velvet"$KVALUE"" > "$samplename"_assemblies.txt
-	awk '{ORS=""}/velvetg/{print $2","}/max/{print $4",",$9,$11,$13"\n"}' "$samplename"_"$KVALUE"/*Log >> $Statsfile
-	Velvet_assemble.sh -k "$LowerK" "$samplename" "$samplename"_R1.fastq.gz "$samplename"_R2.fastq.gz
 
-	Assembly2=$(readlink -f "$samplename"_"$LowerK"/*_contigs.fa)
-	echo -e "$Assembly2""\t"$samplename"_Velvet"$LowerK"" >> "$samplename"_assemblies.txt
+	Velvet_assemble.sh -k "$KVALUE" -c "$CUTOFF" "$samplename" "$samplename"_R1.fastq.gz "$samplename"_R2.fastq.gz
+
+	echo -e count=3 > "$samplename"_assemblies.txt
+	Assembly1=$(readlink -f "$samplename"_"$KVALUE"/*_contigs.fa)
+	echo -e data="$Assembly1"",title="$samplename"_Velvet"$KVALUE"" >> "$samplename"_assemblies.txt
+	awk '{ORS=""}/velvetg/{print $2","}/max/{print $4",",$9,$11,$13"\n"}' "$samplename"_"$KVALUE"/*Log >> $Statsfile
+
+# Assemble sample using mid k-mer value
+echo "Assembling sample "$Count": "$samplename" with K=$MidK and cutoff=$CUTOFF"
+
+	Velvet_assemble.sh -k "$MidK" -c "$CUTOFF" "$samplename" "$samplename"_R1.fastq.gz "$samplename"_R2.fastq.gz
+
+	Assembly2=$(readlink -f "$samplename"_"$MidK"/*_contigs.fa)
+	echo -e data="$Assembly2"",title="$samplename"_Velvet"$MidK"" >> "$samplename"_assemblies.txt
+	awk '{ORS=""}/velvetg/{print $2","}/max/{print $4",",$9,$11,$13"\n"}' "$samplename"_"$MidK"/*Log >> $Statsfile
+
+# Assemble sample using lowest k-mer value
+echo "Assembling sample "$Count": "$samplename" with K=$MidK and cutoff=$CUTOFF"
+
+	Velvet_assemble.sh -k "$LowerK" -c "$CUTOFF" "$samplename" "$samplename"_R1.fastq.gz "$samplename"_R2.fastq.gz
+
+	Assembly3=$(readlink -f "$samplename"_"$LowerK"/*_contigs.fa)
+	echo -e data="$Assembly3"",title="$samplename"_Velvet"$LowerK"" >> "$samplename"_assemblies.txt
+	echo -e Master_file="$samplename"_contigs.fa >> "$samplename"_assemblies.txt
 	awk '{ORS=""}/velvetg/{print $2","}/max/{print $4",",$9,$11,$13"\n"}' "$samplename"_"$LowerK"/*Log >> $Statsfile
+
+# run CISA to merge different k-mer assemblies
+	echo "Merging Assemblies for sample "$Count": "$samplename""
+	Merge.py "$samplename"_assemblies.txt
+	genome=$(awk '/Whole/{print $4}' Merge_info | awk '$0>x{x=$0};END{print x}')
+	echo genome="$genome" > "$samplename"_CISA.config
+	echo infile="$samplename"_contigs.fa >> "$samplename"_CISA.config
+	echo outfile="$samplename"_CISA.fa >> "$samplename"_CISA.config
+	echo CISA=/usr/local/bioinf/CISA1.3 >> "$samplename"_CISA.config
+	CISA.py "$samplename"_CISA.config
+
 
 	rm *.gz
 	cd ..
