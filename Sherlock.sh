98623307177cd37c5f6ce39a6d2a12c2d8c4676b
ellisrichardj <richard.ellis@apha.gsi.gov.uk>
2015-06-11 22:51:18 +0100

    significant changes to blast and incoperating IterMap

--- Sherlock.sh
+++ Sherlock.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
 set -e
 
-# Analysis steps required to find unknown infectious agent using Illumina shotgun sequence data from DNA/RNA extracted from host tissue
+# Analysis steps required to find unknown infectious agent using Illumina shotgun sequence data from DNA/RNA extracted from host tissue.  Initially the host sequences are removed by mapping to the host (or closely related) genome.  
 
-# Pipeline by ellisrichardj making use of some scripts written by Javier Nunez
+# Pipeline by ellisrichardj making use of a variety of standard bioinformatic tools
 
 # Software required:
 	# bwa - 0.7.5a or above
 	# samtools
-	# bam2fastx (part of bamUtil package)
-	# velvet
-	# fastx-toolkit
-	# Mira (includes fastqselect tool)
+	# velvet (must be compiled for multithreading and use of k-mers of at least 101) 
 	# Trimmomatic
-	# viralDetContigs_150514.py
-# Ensure these are in PATH or symbolic links are in a folder that is in your PATH
+	# BLAST+
+	# IterMap.sh (available at https://github.com/ellisrichardj/csu_scripts)
+	# vcf2consensus.pl (also available at https://github.com/ellisrichardj/csu_scripts)
+
+# Ensure that each of these are in PATH or symbolic links are in a folder that is in your PATH
 
 # Version 0.1.1 May 2014 Initial version
 # Version 0.1.2 25/11/14 Improved Usage guidance, added function to indicate time taken for analysis
 # Version 0.1.3 04/03/15 Perform assembly directly from bam file rather than converting back fastq
+# Version 0.1.4 16/04/15 symlink to host genome instead of copying to local directory
+# Version 0.1.5 19/05/15 change underlying viralDetContigs to avoid blast segmentation fault (limit output hits to 5)
+#			symlinks to reference database and query contigs/reads
+# Version 0.1.6 20/05/15 uses exisiting bwa index if it exists
+# Version 0.1.7 29/05/15 blast directly from this script without calling additional python script
+# Version 0.2.0 01/06/15 Revised to incorporate IterMap; mapping raw data to best hit(s) from blast search
+#			automatically generates output directory and file names based on input file names and accession numbers
 
 # set defaults for the options
 KVALUE=101
 CUTOFF=auto
 OutputDir=Sherlock_output
-BlastlngthThresh=0.5
-BlastSimThresh=0.2
-Blast_e_value=0.001
+#BlastlngthThresh=0.5
+#BlastSimThresh=0.2
+Blast_e_value=0.0001
+
 Start=$(date +%s)
 
 # parse the options
-while getopts 's:h:e:r:l:o:c:k:' opt ; do
+while getopts 'H:e:r:c:k:' opt ; do
   case $opt in
-    s) BlastSimThresh=$OPTARG ;;
-    h) PathToHOST=$OPTARG ;;
+#    s) BlastSimThresh=$OPTARG ;;
+    H) PathToHOST=$OPTARG ;;
     e) Blast_e_value=$OPTARG ;;
     r) PathToSearchData=$OPTARG ;;
-    l) BlastlngthThresh=$OPTARG ;;
-    o) OutputDir=$OPTARG ;;
+#    l) BlastlngthThresh=$OPTARG ;;
+#    o) OutputDir=$OPTARG ;;
     c) CUTOFF=$OPTARG ;;
     k) KVALUE=$OPTARG ;;
   esac
@@ -47,62 +55,73 @@ shift $((OPTIND-1))
 # check for mandatory positional parameters
 if [ $# -lt 2 ]; then
   echo "
-Usage: $0 [options] -h path to HOST genome -r path to Reference Database -o Output OutputDirectory name Read1 Read2"
+Usage: $0 [options] -H path to HOST genome -r path to Reference Database Read1.fastq.gz Read2.fastq.gz"
   echo "Options: De Novo assembly: -k Velvet kmervalue (default = 101) | -c Velvet cov_cutoff (default = auto)"
-  echo "Options: Blast Output: -l minimum proportion of contig length (default = 0.5) | -s  minimum similarity threshold for output (default = 0.2) | -e Blast e value (default = 0.001)
+  echo "Options: Blast Output: -e Blast e value (default = 0.0001)
 "
   exit 1
 fi
 LEFT="$1"
 RIGHT="$2"
 
+threads=$(grep -c ^processor /proc/cpuinfo)
+
+# Get sample/reference/host names from file names
+	sfile1=$(basename "$LEFT")
+	sfile2=$(basename "$RIGHT")
+	samplename=${sfile1%%_*}
+
+	ref=$(basename "$PathToSearchData")
+	refname=${ref%%.*}
+
+	host=$(basename "$PathToHOST")
+	hostname=${host%%.*}
+
 # Create output OutputDirectory
+OutputDir="$samplename"_Sherlock_"$refname"
 mkdir "$OutputDir"
 echo "Getting host genome"
-cp "$PathToHOST" "$OutputDir"/HostGenome.fa
 
-# Generate bwa index of host genome
-echo "Generating index of host genome" 
-bwa index "$OutputDir"/HostGenome.fa
+# Generate bwa index of host genome if it doesn't already exist
+if [ -f "$PathToHOST".sa ]
+then 	echo "Using existing host index"
+else 	echo "Generating local index of host genome" 
+	ln -s "$(readlink -f "$PathToHOST")" "$OutputDir"/HostGenome.fa
+	bwa index "$OutputDir"/HostGenome.fa
+	PathToHOST="$OutputDir"/HostGenome.fa
+fi
 
 # Map raw data to host genome
 echo "Mapping raw reads to host reference genome"
-bwa mem -t 6 "$OutputDir"/HostGenome.fa "$LEFT" "$RIGHT" | samtools view -Su - | samtools sort - "$OutputDir"/"$OutputDir"_Host_map_sorted
+bwa mem -t "$threads" "$PathToHOST" "$LEFT" "$RIGHT" | samtools view -Su - | samtools sort - "$OutputDir"/"$samplename"_"$hostname"_map_sorted
 
 # Extract sequence reads that do not map to the host genome
-samtools view -b -f 4 "$OutputDir"/"$OutputDir"_Host_map_sorted.bam > "$OutputDir"/"$OutputDir"_nonHost.bam
+samtools view -b -f 4 "$OutputDir"/"$samplename"_"$hostname"_map_sorted.bam > "$OutputDir"/"$samplename"_nonHost.bam
 
-# Convert non-mapping reads back to fastq format and extract all pairs
-#echo "Extracting non-host raw data"
-#bam2fastx -a -o "$OutputDir"/"$OutputDir"_nonHost.fasta "$OutputDir"/"$OutputDir"_nonHost.bam
-#fasta_formatter -t -i "$OutputDir"/"$OutputDir"_nonHost.fasta -o "$OutputDir"/"$OutputDir"_Sequences_tab.txt
-#awk < "$OutputDir"/"$OutputDir"_Sequences_tab.txt '{print $1}' > "$OutputDir"/"$OutputDir"_Sequences.lst
-#rm "$OutputDir"/"$OutputDir"_nonHost.fasta
-#rm "$OutputDir"/"$OutputDir"_Sequences_tab.txt
-#rm "$OutputDir"/HostGenome.*
+# Denovo assembly of non-host reads
+velveth "$OutputDir"/"$samplename"_nonHost_"$KVALUE" "$KVALUE" -shortPaired -bam "$OutputDir"/"$samplename"_nonHost.bam
+velvetg "$OutputDir"/"$samplename"_nonHost_"$KVALUE" -exp_cov auto -cov_cutoff "$CUTOFF" -ins_length 300 -clean yes -unused_reads yes
+#cat "$OutputDir"/"$samplename"_nonHost_"$KVALUE"/*.fa > "$OutputDir"/"$samplename"_nonHost_"$KVALUE"/contigs_singletons.fa
 
-#gunzip -c "$LEFT" > All_R1.fastq
-#fastqselect -infile All_R1.fastq -name "$OutputDir"/"$OutputDir"_Sequences.lst -outfile "$OutputDir"/"$OutputDir"_R1_nonHost.fastq
-#gunzip -c "$RIGHT" > All_R2.fastq
-#fastqselect -infile All_R2.fastq -name "$OutputDir"/"$OutputDir"_Sequences.lst -outfile "$OutputDir"/"$OutputDir"_R2_nonHost.fastq
-#rm All_R1.fastq
-#rm All_R2.fastq
-
-# Quality trimming and Denovo assembly of non-host reads
-#let Trimlgth="$KVALUE"+10
-#trimmomatic-0.30.jar PE -threads 6 -phred33 "$OutputDir"/"$OutputDir"_R1_nonHost.fastq "$OutputDir"/"$OutputDir"_R2_nonHost.fastq  "$OutputDir"/R1_nonhost_trim_paired.fastq "$OutputDir"/R1_unpaired.fastq "$OutputDir"/R2_nonhost_trim_paired.fastq "$OutputDir"/R2_unpaired.fastq ILLUMINACLIP:/home/sequence/ReferenceSequences/adapter.fasta:1:30:10:5:true MAXINFO:"$Trimlgth":0.5 MINLEN:"$Trimlgth"
-#rm "$OutputDir"/R1_unpaired.fastq
-#rm "$OutputDir"/R2_unpaired.fastq
-velveth "$OutputDir"/"$OutputDir"_"$KVALUE" "$KVALUE" -shortPaired -bam "$OutputDir"/"$OutputDir"_nonHost.bam
-velvetg "$OutputDir"/"$OutputDir"_"$KVALUE" -exp_cov auto -cov_cutoff "$CUTOFF" -ins_length 300 -clean yes -read_trkg yes -amos_file yes -unused_reads yes
-cat "$OutputDir"/"$OutputDir"_"$KVALUE"/*.fa > "$OutputDir"/"$OutputDir"_"$KVALUE"/contigs_singletons.fa
-
-# Now run Blast of output contigs and singleton reads against database of choice.  If you have some idea of what you are looking for you can limit the search to a particular taxonomic group (e.g. Rhabdoviridae).
+# Now run Blast of output contigs (potentially including unassembled singleton reads) against database of choice.  If you have some idea of what you are looking for you can limit the search to a particular taxonomic group (e.g. Rhabdoviridae) providing you have a database for it.
 
 cd "$OutputDir"
-cp "$OutputDir"_"$KVALUE"/contigs_singletons.fa .
-cp "$PathToSearchData" ./Reference.fasta
-viralDetContigs_150514.py "$PWD" Reference.fasta "$Blast_e_value" contigs_singletons.fa "$BlastlngthThresh" "$BlastSimThresh"
+#ln -s "$(readlink -f "$samplename"_nonHost_"$KVALUE"/contigs_singletons.fa)" contigs_singletons.fa
+ln -s "$(readlink -f "$PathToSearchData")" Reference.fasta
+
+makeblastdb -in Reference.fasta -parse_seqids -dbtype nucl
+echo "BLASTing contigs against selected database"
+blastall -a "$threads" -b 5 -p blastn -d Reference.fasta -i "$samplename"_nonHost_"$KVALUE"/contigs.fa -o "$samplename"_"$refname"_crunch.txt -m 8 -e "$Blast_e_value"
+
+# Extract the single sequence from blast output corresponding to the highest scoring match
+sort -k12,12 -rn "$samplename"_"$refname"_crunch.txt | head -1 - | awk '{print $2}' - > top_match.txt
+AccNo=$(sed -e 's/.*\([A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/g'  top_match.txt)
+blastdbcmd -db Reference.fasta -entry_batch top_match.txt > "$AccNo".fas
+
+# Map original data to selected reference sequences and generate new consensus
+IterMap.sh -i4 "$AccNo".fas "$LEFT" "$RIGHT"
+
+
 End=$(date +%s)
 TimeTaken=$((End-Start))
 echo "Results are in: "$OutputDir""
